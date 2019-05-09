c2681354be98123bc71daf599cfd6cbf3c62cb30
dd <dd@dd.com>
2019-05-09 14:36:19 +0900

    acceptence

--- acceptance_test.sh
+++ acceptance_test.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \    # true when it runs on host
-test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
+#test $(curl --silent localhost:8090/sum?a=1\&b=2) -eq 3 || \    # true when it runs on host
+#test $(curl --silent $(docker network inspect -f '{{range .IPAM.Config}}{{.Gateway}}{{end}}' bridge):8090/sum?a=1\&b=2) -eq 3  # true on docker
+
+CALCULATOR_PORT=$(docker-compose port calculator 8090 | cut -d: -f2)
+test $(curl --silent -X get localhost:$CALCULATOR_PORT/sum?a=1&b=2) -eq 3
